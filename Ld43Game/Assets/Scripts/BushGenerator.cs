fb731c262f9f0b77eab40d4ec8615b15451d61af
Albert <albert24lindberg@gmail.com>
2018-12-02 19:57:45 +0200

    stuff

---
+++ Ld43Game/Assets/Scripts/BushGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushGenerator : MonoBehaviour {
+
+    [SerializeField] private GameObject bushType;
+    [SerializeField] private GameObject palmType;
+    [SerializeField] private float maxBushes;
+
+    [SerializeField] private float maxX;
+    [SerializeField] private float maxZ;
+
+    [SerializeField] private float BushY;
+    [SerializeField] private float PalmY;
+
+    private float bushes = 0;
+
+    // Use this for initialization
+    void Start () {
+        SpawnBushes();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void SpawnBushes()
+    {
+        while (bushes < maxBushes)
+        {
+
+            GameObject d = GameObject.Instantiate(bushType, new Vector3(Random.Range(-maxX, maxX), BushY, Random.Range(-maxZ, maxZ)), transform.rotation);
+            GameObject p = GameObject.Instantiate(palmType, new Vector3(Random.Range(-maxX, maxX), PalmY, Random.Range(-maxZ, maxZ)), transform.rotation);
+            bushes++;
+        }
+        bushes = 0;
+
+    }
+}
---
+++ Ld43Game/Assets/SimpleCarController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour {
+
+    [SerializeField] private float power;
+    [SerializeField] private float torque;
+
+
+    private Rigidbody rb;
+    private float distToGround;
+	// Use this for initialization
+	void Start () {
+        distToGround = transform.position.y;
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsGrounded())
+        {
+            Debug.Log("HERE");
+            rb.AddForce(transform.forward * power * Input.GetAxis("Vertical"));
+            rb.AddTorque(transform.up * torque * Input.GetAxis("Horizontal"));
+        }
+	}
+
+    private bool IsGrounded()
+    {
+        if(transform.position.y <= distToGround)
+        {
+            return true;
+        }
+        return false;
+    }
+}
